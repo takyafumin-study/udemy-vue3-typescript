43d228b86c2fc59df684b6fd33d15fe9afd3e045
takyafumin <takyafumin@gmail.com>
2024-01-12 20:13:58 +0900

    Section4, Add v-bind for Payment Component

--- app/src/components/Payment.vue
+++ app/src/components/Payment.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
+const itemName1 = 'Desk';
+const itemName2 = 'Bike';
+
+const price1 = 40000;
+const price2 = 20000;
+
+const url1 = 'https://www.amazon.co.jp/dp/B07CMTX1VB';
+const url2 = 'https://www.amazon.co.jp/dp/B000ZKRE9U';
 
 </script>
 
 <template>
     <div class="container">
-        <h1>最近の支出</h1>
+        <h1>Payment</h1>
         <div class="payment">
-            <label>カレーライス</label>
-            <label>400円</label>
+            <label>{{ itemName1 }}</label>
+            <label>{{ price1 }}円</label>
+            <a v-bind:href="url1">Amazon</a>
+        </div>
+        <div class="payment">
+            <label>{{ itemName2 }}</label>
+            <label>{{ price2 }}円</label>
+            <a v-bind:href="url2">Amazon</a>
         </div>
     </div>
 </template>
@@ -27,6 +41,7 @@
     height: 80px;
     width: 400px;
     background-color: aliceblue;
+    margin-bottom: 8px;
 }
 
 label {
