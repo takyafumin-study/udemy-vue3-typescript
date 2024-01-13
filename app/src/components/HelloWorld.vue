3246a6d902fdb4752d0ad251a195045f3fa8f3dc
takyafumin <takyafumin@gmail.com>
2024-01-13 17:44:43 +0900

    Section5, Add Tweet List add function

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -2,10 +2,24 @@
 import { ref } from "vue";
 
 const tweets = ref<Array<string>>(["Tweet1", "Tweet2", "Tweet3"]);
+const tweet = ref<string>("");
+
+/**
+ * submit
+ */
+const onSubmit = () => {
+  // console.log("Tweeted!");
+  tweets.value.push(tweet.value);
+  tweet.value = "";
+};
 </script>
 
 <template>
   <h1>Twitter</h1>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="tweet" />
+    <button type="submit">Tweet</button>
+  </form>
   <ul>
     <li v-for="(tweet, key) in tweets" :key="key">{{ tweet }}</li>
   </ul>
