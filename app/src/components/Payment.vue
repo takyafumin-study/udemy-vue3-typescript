2b34edb6e74463af1a8ee172d7e5530577107d38
takyafumin <takyafumin@gmail.com>
2024-01-13 15:35:29 +0900

    Section4, format source code

--- app/src/components/Payment.vue
+++ app/src/components/Payment.vue
@@ -1,57 +1,60 @@
 <script setup lang="ts">
-const itemName1 = 'Desk';
-const itemName2 = 'Bike';
+const itemName1 = ref("Desk");
+const itemName2 = "Bike";
 
 const price1 = 40000;
 const price2 = 20000;
 
-const url1 = 'https://www.amazon.co.jp/dp/B07CMTX1VB';
-const url2 = 'https://www.amazon.co.jp/dp/B000ZKRE9U';
+const url1 = "https://www.amazon.co.jp/dp/B07CMTX1VB";
+const url2 = "https://www.amazon.co.jp/dp/B000ZKRE9U";
 
 const buy = (itemName: string) => {
-    alert('Aer you sure to buy ' + itemName + "?");
-}
+  alert("Aer you sure to buy " + itemName + "?");
+};
 
+const input = (event: any) => {
+  console.log(event.target.value);
+};
 </script>
 
 <template>
-    <div class="container">
-        <h1>Payment</h1>
-        <div class="payment">
-            <label>{{ itemName1 }}</label>
-            <label>{{ price1 }}円</label>
-            <a v-bind:href="url1">Amazon</a>
-            <button v-on:click="buy(itemName1)">BUY</button>
-        </div>
-        <div class="payment">
-            <label>{{ itemName2 }}</label>
-            <label>{{ price2 }}円</label>
-            <a v-bind:href="url2">Amazon</a>
-            <button v-on:click="buy(itemName2)">BUY</button>
-        </div>
+  <div class="container">
+    <h1>Payment</h1>
+    <input type="text" v-on:input="input" />
+    <div class="payment">
+      <label>{{ itemName1 }}</label>
+      <label>{{ price1 }}円</label>
+      <a v-bind:href="url1">Amazon</a>
+      <button v-on:click="buy(itemName1)">BUY</button>
     </div>
+    <div class="payment">
+      <label>{{ itemName2 }}</label>
+      <label>{{ price2 }}円</label>
+      <a v-bind:href="url2">Amazon</a>
+      <button v-on:click="buy(itemName2)">BUY</button>
+    </div>
+  </div>
 </template>
-
 
 <style scoped>
 .container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .payment {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 80px;
-    width: 400px;
-    background-color: aliceblue;
-    margin-bottom: 8px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 80px;
+  width: 400px;
+  background-color: aliceblue;
+  margin-bottom: 8px;
 }
 
 label {
-    font-size: 20px;
-    font-weight: bold;
+  font-size: 20px;
+  font-weight: bold;
 }
 </style>
