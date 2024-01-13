5a9d54e0aff79258fb4f938a3012f91f14e985de
takyafumin <takyafumin@gmail.com>
2024-01-13 16:53:15 +0900

    Section4, Add computed function for Payment Component

--- app/src/components/Payment.vue
+++ app/src/components/Payment.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
-import { ref, reactive } from "vue";
-
-const itemName2 = ref<string>("Bike");
+import { ref, reactive, computed } from "vue";
 
 const item1 = reactive({
   name: "Desk",
   price: 40000,
 });
 
-const price2 = 20000;
+const budget = 50000;
 
-const url1 = "https://www.amazon.co.jp/dp/B07CMTX1VB";
-const url2 = "https://www.amazon.co.jp/dp/B000ZKRE9U";
+/**
+ * Price label
+ */
+const priceLabel = computed(() => {
+  return item1.price > budget ? "too expensive!" : item1.price + "円";
+});
+
+const itemName2 = ref<string>("Bike");
+const price2 = 20000;
 
 const buy = (itemName: string) => {
   alert("Aer you sure to buy " + itemName + "?");
@@ -34,14 +39,12 @@ const clear = () => {
     <button v-on:click="clear">Clear</button>
     <div class="payment">
       <label>{{ item1.name }}</label>
-      <label>{{ item1.price }}円</label>
-      <a v-bind:href="url1">Amazon</a>
+      <label>{{ priceLabel }}</label>
       <button v-on:click="buy(item1.name)">BUY</button>
     </div>
     <div class="payment">
       <label>{{ itemName2 }}</label>
       <label>{{ price2 }}円</label>
-      <a v-bind:href="url2">Amazon</a>
       <button v-on:click="buy(itemName2)">BUY</button>
     </div>
   </div>
