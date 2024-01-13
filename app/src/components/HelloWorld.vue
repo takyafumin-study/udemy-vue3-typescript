70081a3c2b2ad67cc25e22ccba8ab2f1dcbf0f18
takyafumin <takyafumin@gmail.com>
2024-01-13 17:38:42 +0900

    Section5, Add List for HelloWorld Component

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const tweets = ref<Array<string>>(["Tweet1", "Tweet2", "Tweet3"]);
+</script>
 
 <template>
   <h1>Twitter</h1>
+  <ul>
+    <li v-for="(tweet, key) in tweets" :key="key">{{ tweet }}</li>
+  </ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  margin-top: 1rem;
+}
+</style>
